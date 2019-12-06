3e4ddb1105b107bc2b8bad929da79f293482fa89
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-06 11:28:28 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -2,60 +2,71 @@
 'use strict';
 
 var ths = document.getElementsByTagName('th'); 
-var i;
 var sortOrder = 1; //1:昇順、-1:降順
 
-for (i = 0; i < ths.length; i++) {
-ths[i].addEventListener('click', function() {
-
-var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr')); 
-
-var col = this.cellIndex;  
-var type = this.dataset.type 
-
-rows.sort(function(a, b) {
-  if (type === "number") {
-    var _a = a.children[col].textContent * 1; 
-    var _b = b.children[col].textContent * 1;
-  }
-
-  if (type === "string") {
-    var _a = a.children[col].textContent.toLowerCase(); 
-    var _b = b.children[col].textContent.toLowerCase();
-  }
-
-  if (_a < _b) { 
-    return -1 * sortOrder;
-  }
-  if (_a > _b) {
-    return 1 * sortOrder;
-  }
-  return 0;
-});
-
-var tbody = document.querySelector('tbody');
-
-
+function rebuildTbody(rows) {
+  var tbody = document.querySelector('tbody');
+  var i;
 while (tbody.firstChild) {
   tbody.removeChild(tbody.firstChild);
 }
-
-var j;
-for (j = 0; j < rows.length; j++) {
-  tbody.appendChild(rows[j]);
+for (i = 0; i < rows.length; i++) {
+  tbody.appendChild(rows[i]);
+}
 }
 
-var k;
+function updateClassName(th) {
+  var k;
 for (k = 0; k < ths.length; k++) {
   ths[k].className = '';  //クラスをリセット
 }
-
-//sortOrder に応じて ClassName をつけていきたいので、sortOrder がもし 1 だったら昇順という意味で 'asc' という className、そうではなかったら降順なので 'desc' という className をつけてあげる。
-this.className = sortOrder === 1 ? 'asc' : 'desc';
-
-sortOrder *= -1; //反転させる
-
-});
+th.className = sortOrder === 1 ? 'asc' : 'desc';
 }
+
+function compare(a, b, col, type) {
+  var _a = a.children[col].textContent;
+  var _b = b.children[col].textContent;
+  if (type === "number") {
+     _b = _b * 1;
+     _a = _a * 1; 
+  } else if  (type === "string") {
+     _a = _a.toLowerCase(); 
+     _b = _b.toLowerCase();
+  }
+
+  if (_a < _b) { 
+    return -1;
+  }
+  if (_a > _b) {
+    return 1;
+  }
+  return 0;
+}
+
+function sortRows(th) {
+  var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr')); 
+  var col = th.cellIndex;  
+  var type = th.dataset.type 
+
+  rows.sort(function(a, b) {
+    return compare(a, b, col, type) * sortOrder;
+  });
+  return rows;
+}
+
+function setup() {
+  var i;
+  for (i = 0; i < ths.length; i++) {
+    ths[i].addEventListener('click', function() {
+    var rows;
+    rows = sortRows(this);
+    rebuildTbody(rows);
+    updateClassName(this);
+    sortOrder *= -1; //反転させる
+    });
+    }
+}
+
+setup();
 
 })();
